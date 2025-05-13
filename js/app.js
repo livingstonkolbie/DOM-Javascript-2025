5e0a5b337f9c102329d40726195755abc2387a8f
livingstonkolbie <kolbie.livingston@mscoding.org>
2025-05-13 15:04:59 -0600

    DOM lesson problems

---
+++ js/app.js
@@ -0,0 +1,88 @@
+
+
+// UPDATING AND GRABBING NODES from HTML document
+// Grabs our "h1" tag from our index.html
+let myName = document.getElementById("myName");
+console.log(myName);
+
+
+
+// Updates h1 text and replaces it with set string ("Kolbie Livingston")
+myName.innerText = "Kolbie Livingston";
+
+const thingsILike = ["Learning", "Food", "Books"];
+
+
+
+// Grabs group list of ELEMENT NODES and converts to an ARRAY list
+let liList = document.querySelectorAll(".my-items");
+
+// querySelector selects one
+let liSecond = document.querySelector("#myList li:nth-child(2)");
+console.log(liList);
+console.log(liSecond);
+
+
+
+for (let i = 0; i < liList.length; i++) {
+    console.log(liList[i]);
+    liList[i].innerText = thingsILike[i];
+}
+
+// BELOW IS DOING THE SAME THING- JUST SIMPLIFIED INEFFICIENT VERSION
+// liList[0].innerText = thingsILike[0];
+// liList[1].innerText = thingsILike[1];
+// liList[2].innerText = thingsILike[2];
+
+
+
+// Creating Tags and Elements
+
+// Grabbing Ul Node Element to use appendChild()
+let parentEl = document.getElementById("myList");
+console.log(parentEl);
+
+// Created a li tag below 
+let liItem = document.createElement("li");
+// console.log(liItem); // <li></li>
+
+liItem.innerText = "Sports";
+console.log(liItem); // <li>Sports</li>
+
+
+// Appending the "liItem" node to our HTML page
+
+parentEl.appendChild(liItem);
+
+
+// DOM styling
+
+let paragragph = document.querySelector("p");
+console.log(paragragph);
+
+paragragph.style.fontSize = "25px";
+paragragph.style.backgroundColor = "tomato";
+
+
+
+// DOM EventListeners
+
+// EventListeners are waiting for a type of event to fire off
+
+let clickButton = document.querySelector("#here");
+// console.log(clickButton);
+
+//BELOW clickButton Node element ... (the event, function(without the parenthese))
+clickButton.addEventListener("click", colorList);
+
+function colorList() {
+    // "liList" variable is only coloring the first 3 li's
+    // Re-select that node list to get all 4 li's
+    let reList = document.querySelectorAll("li");
+    console.log(reList);
+
+    for (let i = 0; i < reList.length; i++) {
+
+        reList[i].style.color = "purple";
+    }
+}
